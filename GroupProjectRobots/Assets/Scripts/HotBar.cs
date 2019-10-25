25c09678dc0943ee766bd48bb5cbf3e17f7572cf
Taran Zorn <21.taran.zorn@ksd.org>
2019-10-25 08:46:39 -0700

    Fixed Sprint/Added Inventory

---
+++ GroupProjectRobots/Assets/Scripts/HotBar.cs
@@ -0,0 +1,325 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotBar : MonoBehaviour
+{
+    public GameObject Purple;
+    public GameObject Purple1;
+    public GameObject Purple2;
+    public GameObject Purple3;
+    public GameObject Blue;
+    public GameObject Blue1;
+    public GameObject Blue2;
+    public GameObject Blue3;
+    public GameObject Yellow;
+    public GameObject Yellow1;
+    public GameObject Yellow2;
+    public GameObject Yellow3;
+    public GameObject Green;
+    public GameObject Green1;
+    public GameObject Green2;
+    public GameObject Green3;
+    public bool inventory1 = false;
+    public bool inventory2 = false;
+    public bool inventory3 = false;
+    public bool inventory4 = false;
+    public static bool inventoryfull = false;
+    public int inventorycounter = 0;
+    public string inventoryitem1;
+    public string inventoryitem2;
+    public string inventoryitem3;
+    public string inventoryitem4;
+    public Transform player;
+    public GameObject yellow1;
+    public GameObject blue1;
+    public GameObject green1;
+    public GameObject purple1;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+        if(inventorycounter == 4)
+        {
+            inventoryfull = true;
+        }
+        else
+        {
+            inventoryfull = false;
+        }
+    }
+    public void PurplePickup()
+    {
+        if (!inventory1)
+        {
+            Purple.SetActive(true);
+            inventory1 = true;
+            inventorycounter += 1;
+            inventoryitem1 = "purple";
+        }
+        else if (!inventory2)
+        {
+            Purple1.SetActive(true);
+            inventory2 = true;
+            inventorycounter += 1;
+            inventoryitem2 = "purple";
+        }
+        else if (!inventory3)
+        {
+            Purple2.SetActive(true);
+            inventory3 = true;
+            inventorycounter += 1;
+            inventoryitem3 = "purple";
+        }
+        else if (!inventory4)
+        {
+            Purple3.SetActive(true);
+            inventory4 = true;
+            inventorycounter += 1;
+            inventoryitem4 = "purple";
+        }
+
+    }
+    public void BluePickup()
+    {
+        if (!inventory1)
+        {
+            Blue.SetActive(true);
+            inventory1 = true;
+            inventorycounter += 1;
+            inventoryitem1 = "blue";
+        }
+        else if (!inventory2)
+        {
+            Blue1.SetActive(true);
+            inventory2 = true;
+            inventorycounter += 1;
+            inventoryitem2 = "blue";
+        }
+        else if (!inventory3)
+        {
+            Blue2.SetActive(true);
+            inventory3 = true;
+            inventorycounter += 1;
+            inventoryitem3 = "blue";
+        }
+        else if (!inventory4)
+        {
+            Blue3.SetActive(true);
+            inventory4 = true;
+            inventorycounter += 1;
+            inventoryitem4 = "blue";
+        }
+    }
+    public void GreenPickup()
+    {
+        if (!inventory1)
+        {
+            Green.SetActive(true);
+            inventory1 = true;
+            inventorycounter += 1;
+            inventoryitem1 = "green";
+        }
+        else if (!inventory2)
+        {
+            Green1.SetActive(true);
+            inventory2 = true;
+            inventorycounter += 1;
+            inventoryitem2 = "green";
+        }
+        else if (!inventory3)
+        {
+            Green2.SetActive(true);
+            inventory3 = true;
+            inventorycounter += 1;
+            inventoryitem3 = "green";
+        }
+        else if (!inventory4)
+        {
+            Green3.SetActive(true);
+            inventory4 = true;
+            inventorycounter += 1;
+            inventoryitem4 = "green";
+        }
+    }
+    public void YellowPickup()
+    {
+        if (!inventory1)
+        {
+            Yellow.SetActive(true);
+            inventory1 = true;
+            inventorycounter += 1;
+            inventoryitem1 = "yellow";
+        }
+        else if (!inventory2)
+        {
+            Yellow1.SetActive(true);
+            inventory2 = true;
+            inventorycounter += 1;
+            inventoryitem2 = "yellow";
+        }
+        else if (!inventory3)
+        {
+            Yellow2.SetActive(true);
+            inventory3 = true;
+            inventorycounter += 1;
+            inventoryitem3 = "yellow";
+        }
+        else if (!inventory4)
+        {
+            Yellow3.SetActive(true);
+            inventory4 = true;
+            inventorycounter += 1;
+            inventoryitem4 = "yellow";
+        }
+    }
+    public void Hotbar1()
+    {
+        if(inventoryitem1 == "purple")
+        {
+            Purple.SetActive(false);
+            inventory1 = false;
+            inventorycounter -= 1;
+            Instantiate(purple1, player.position, player.rotation);
+
+        }
+        else if(inventoryitem1 == "blue")
+        {
+            Blue.SetActive(false);
+            inventory1 = false;
+            inventorycounter -= 1;
+            Instantiate(blue1, player.position, player.rotation);
+        }
+        else if (inventoryitem1 == "yellow")
+        {
+            Yellow.SetActive(false);
+            inventory1 = false;
+            inventorycounter -= 1;
+            Instantiate(yellow1, player.position, player.rotation);
+        }
+        else if (inventoryitem1 == "green")
+        {
+            Green.SetActive(false);
+            inventory1 = false;
+            inventorycounter -= 1;
+            Instantiate(green1, player.position, player.rotation);
+        }
+    }
+    public void Hotbar2()
+    {
+        {
+            if (inventoryitem2 == "purple")
+            {
+                Purple1.SetActive(false);
+                inventory2 = false;
+                inventorycounter -= 1;
+                Instantiate(purple1, player.position, player.rotation);
+            }
+            else if (inventoryitem2 == "blue")
+            {
+                Blue1.SetActive(false);
+                inventory2 = false;
+                inventorycounter -= 1;
+                Instantiate(blue1, player.position, player.rotation);
+
+            }
+            else if (inventoryitem2 == "yellow")
+            {
+                Yellow1.SetActive(false);
+                inventory2 = false;
+                inventorycounter -= 1;
+                Instantiate(yellow1, player.position, player.rotation);
+            }
+            else if (inventoryitem2 == "green")
+            {
+                Green1.SetActive(false);
+                inventory2 = false;
+                inventorycounter -= 1;
+                Instantiate(green1, player.position, player.rotation);
+            }
+        }
+    } 
+    public void Hotbar3()
+    {
+            {
+                if (inventoryitem3 == "purple")
+                {
+                    Purple2.SetActive(false);
+                    inventory3 = false;
+                    inventorycounter -= 1;
+                    Instantiate(purple1, player.position, player.rotation);
+                }
+                else if (inventoryitem3 == "blue")
+                {
+                    Blue2.SetActive(false);
+                    inventory3 = false;
+                    inventorycounter -= 1;
+                    Instantiate(blue1, player.position, player.rotation);
+
+                }
+                else if (inventoryitem3 == "yellow")
+                {
+                    Yellow2.SetActive(false);
+                    inventory3 = false;
+                    inventorycounter -= 1;
+                    Instantiate(yellow1, player.position, player.rotation);
+                }
+                else if (inventoryitem3 == "green")
+                {
+                    Green2.SetActive(false);
+                    inventory3 = false;
+                    inventorycounter -= 1;
+                    Instantiate(green1, player.position, player.rotation);
+                }
+            }
+        }
+    public void Hotbar4()
+    {
+        {
+            if (inventoryitem4 == "purple")
+            {
+                Purple3.SetActive(false);
+                inventory4 = false;
+                inventorycounter -= 1;
+                Instantiate(purple1, player.position, player.rotation);
+
+            }
+            else if (inventoryitem4 == "blue")
+            {
+                Blue3.SetActive(false);
+                inventory4 = false;
+                inventorycounter -= 1;
+                Instantiate(blue1, player.position, player.rotation);
+            }
+            else if (inventoryitem4 == "yellow")
+            {
+                Yellow3.SetActive(false);
+                inventory4 = false;
+                inventorycounter -= 1;
+                Instantiate(yellow1, player.position, player.rotation);
+            }
+            else if (inventoryitem4 == "green")
+            {
+                Green3.SetActive(false);
+                inventory4 = false;
+                inventorycounter -= 1;
+                Instantiate(green1, player.position, player.rotation);
+
+                
+
+            }
+        }
+    }
+}
+
+
+
---
+++ GroupProjectRobots/Assets/Scripts/PlayerPickup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour
+{
+    public HotBar hotbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Purple" && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(collision.gameObject);
+            hotbar.PurplePickup();
+        }
+        if (collision.gameObject.tag == "Green" && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(collision.gameObject);
+            hotbar.GreenPickup();
+        }
+        if (collision.gameObject.tag == "Blue" && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(collision.gameObject);
+            hotbar.BluePickup();
+        }
+        if (collision.gameObject.tag == "Yellow" && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(collision.gameObject);
+            hotbar.YellowPickup();
+        }
+    }
+}
