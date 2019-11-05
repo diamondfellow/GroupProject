4f7ec11eb5d3aa7d5d46d6d4ecf4fd5b1a8422ba
Max Van Den Dikkenberg <21.Max.Van@ksd.org>
2019-11-05 09:55:03 -0800

    Main menu, robits, transitions between scenes

---
+++ GroupProjectRobots/Assets/Scripts/MainHP.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainHP : MonoBehaviour
+{
+    public static int health = 1;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            health = health - 1;
+            if (health < 1)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+}
---
+++ GroupProjectRobots/Assets/Scripts/Outside.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Outside : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Outside")
+        {
+            SceneManager.LoadScene("Outside");
+        }
+    }
+}
