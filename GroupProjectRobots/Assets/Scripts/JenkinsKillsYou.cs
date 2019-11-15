079f21242a08ddddab669e9bdaf887dbab5c3a29
Max Van Den Dikkenberg <21.Max.Van@ksd.org>
2019-11-15 08:41:55 -0800

    Jenkins insta kill, fixed pause menu

---
+++ GroupProjectRobots/Assets/Scripts/JenkinsKillsYou.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class JenkinsKillsYou : MonoBehaviour
+{
+    public static int health = 6;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Jenkins")
+        {
+            health = health - 6;
+            if (health < 1)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+    
+    public void Update()
+    {
+        if (health > 6)
+        {
+            health = 6;
+        }
+    }
+}
